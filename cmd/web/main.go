dced2757633da464ff5f299bb925084c11235cc8
rajnautiyal <nautiyal.rajendra@gmail.com>
2023-12-21 16:07:21 +0000

    add

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github/rajnautiyal/webCourse/cmd/pkg/handler"
+
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handler.Home)
+	http.HandleFunc("/about", handler.About)
 
 	fmt.Printf("starting my application server %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
